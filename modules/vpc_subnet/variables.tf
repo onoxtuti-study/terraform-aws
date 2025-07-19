63c5ead69230e1b7e6d62e0bc117e6d8d9d0a3af
onoxtuti <onozawa.tsubasa@gmail.com>
2025-07-19 22:05:29 +0900

    20250719_01_stg_release

---
+++ modules/vpc_subnet/variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_cidr_block" {
+    type = string
+}
+
+variable "vpc_name" {
+    type = string
+}
+
+variable "subnet_cidr_block" {
+    type = string
+}
+
+variable "subnet_name" {
+    type = string
+}
