6b3b70a22e590bcb2eeb7681f400bea3c77b0201
onoxtuti <onozawa.tsubasa@gmail.com>
2025-07-19 22:34:36 +0900

    terraformのworkspace作成分岐とvpc.tfの環境分岐を追記

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
 module "my_vpc_subnet" {
   source = "./modules/vpc_subnet"
 
-  vpc_cidr_block = terraform.workspace == "stg" ? "10.0.0.0/16" : "11.0.0.0/16"
+  vpc_cidr_block = terraform.workspace == "stg" ? "10.0.0.0/16" : terraform.workspace == "prd" ? "11.0.0.0/16"
   vpc_name = "onozawa-terraform-${terraform.workspace}"
-  subnet_cidr_block = terraform.workspace == "stg" ? "10.0.1.0/24" : "11.0.1.0/24"
+  subnet_cidr_block = terraform.workspace == "stg" ? "10.0.1.0/24" : terraform.workspace == "prd" ? "11.0.1.0/24"
   subnet_name = "DMZ-${terraform.workspace}-1a"
 }
