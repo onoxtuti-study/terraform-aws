d3b1480ca0bdf356d3a3635fa11a7bd030678c97
onoxtuti <onozawa.tsubasa@gmail.com>
2025-07-19 22:59:31 +0900

    triger re re run

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,6 @@
-module "my_vpc_subnet" {
+module "first_vpc_subnet" {
   source = "./modules/vpc_subnet"
 
-  vpc_cidr_block = lookup(var.vpc_cidr_block_map, terraform.workspace, null)
   vpc_name = "onozawa-terraform-${terraform.workspace}"
-  subnet_cidr_block = lookup(var.subnet_cidr_block_map, terraform.workspace, null)
   subnet_name = "DMZ-${terraform.workspace}-1a"
 }
