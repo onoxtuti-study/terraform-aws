cf53ec28c405ef15f1120551016f937f41ef7439
onoxtuti <onozawa.tsubasa@gmail.com>
2025-07-19 22:59:31 +0900

    triger re re run

--- modules/vpc_subnet/variables.tf
+++ modules/vpc_subnet/variables.tf
@@ -9,7 +9,7 @@ variable "vpc_name" {
     type = string
 }
 
-variable "subnet_cidr_block_map" {
+variable "dmz_1a_subnet_cidr_block_map" {
     default = {
         stg = "10.0.1.0/24"
         prd = "11.0.1.0/24"
