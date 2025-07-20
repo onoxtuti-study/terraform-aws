fbc5042fbff208ff28bfef33310d9107293a6694
onoxtuti <onozawa.tsubasa@gmail.com>
2025-07-21 00:39:02 +0900

    create bastion ec2

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -3,16 +3,6 @@ resource "aws_security_group" "bastion" {
   description = "bastion ec2"
   vpc_id      = var.vpc_id
 
-  for_each = toset(var.open_ip)
-
-  ingress {
-    description = "SSH from external"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [each.value]
-  }
-
   egress {
     from_port        = 0
     to_port          = 0
@@ -24,4 +14,20 @@ resource "aws_security_group" "bastion" {
   tags = {
     Name = var.sg_name
   }
+}
+
+resource "aws_security_group_rule" "bastion_ingress" {
+  for_each = toset(var.open_ip)
+
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = [each.value]
+  security_group_id = aws_security_group.bastion.id
+  description       = "SSH from ${each.value}"
+}
+
+output "bastion_id" {
+  value = aws_security_group.bastion.id
 }
--- modules/vpc_subnet/main.tf
+++ modules/vpc_subnet/main.tf
@@ -17,3 +17,7 @@ resource "aws_subnet" "study_dmz_1a" {
 output "vpc_id" {
   value = aws_vpc.study.id
 }
+
+output "subnet_id" {
+  value = aws_subnet.study_dmz_1a.id
+}
