1955a15139d25d1d3f724e2af3df04b1748fdb94
onoxtuti <onozawa.tsubasa@gmail.com>
2025-07-21 15:54:13 +0900

    create bastion

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -15,7 +15,7 @@ resource "aws_iam_role" "role" {
     assume_role_policy = data.aws_iam_policy_document.role.json
 }
 resource "aws_iam_role_policy_attachment" "role_attach" {
-    count = length(var.customer_role_name)
+  count = length(var.customer_role_name)
 
   role = aws_iam_role.role.name
   policy_arn = lookup(var.iam_role_policy_map, var.customer_role_name[count.index], null)
