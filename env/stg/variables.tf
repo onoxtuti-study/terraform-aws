30ffcbc95e7b76122c1c729a8145bead629fb4ef
onoxtuti <onozawa.tsubasa@gmail.com>
2025-07-25 13:16:15 +0900

    change bastion ec2 config

--- env/stg/variables.tf
+++ env/stg/variables.tf
@@ -2,5 +2,6 @@
 # SG import variables
 #---------------------------------------
 variable "bastion_open_ip" {
+  description = "IP addresses allowed in the security group for the bastion"
   type = list(string)
 }
--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,8 +1,10 @@
 variable "ec2_name" {
+    description = "EC2 Name"
     type = string
 }
 
 variable "ami_id" {
+    description = "Base ami id"
     type = map(string)
     default = {
     "al2023" = "ami-0f95ad36d6d54ceba"
@@ -10,17 +12,27 @@ variable "ami_id" {
 }
 
 variable "profile" {
+    description = "use instance profile name"
     type = string
 }
 
 variable "sg_id" {
+    description = "use security group name"
     type = list(string)
 }
 
 variable "subnet_id" {
+    description = "use subnet id"
     type = string
 }
 
 variable "key_name" {
+    description = "use key pair"
     type = string
+}
+
+variable "associate_public_ip_address" {
+    description = "Assign public ip based on environment"
+  type = bool
+  default = false
 }
