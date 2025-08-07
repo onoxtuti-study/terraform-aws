001056a1304aea6a450394420ec9fd90ad462eed
onoxtuti <onozawa.tsubasa@gmail.com>
2025-08-07 11:43:30 +0900

    fix

--- modules/routetable/main.tf
+++ modules/routetable/main.tf
@@ -11,12 +11,6 @@ resource "aws_route" "default_route" {
   gateway_id             = var.gw_id  # NAT Gateway ID (nat-xxxx) も OK
 }
 
-resource "aws_route" "local_route" {
-  route_table_id         = aws_route_table.public_route_table.id
-  destination_cidr_block = var.cidr_block
-  gateway_id             = "local"
-}
-
 resource "aws_route_table_association" "onozawa-terraform-stg_DMZ-stg-1a" {
   subnet_id      = var.subnet_id
   route_table_id = aws_route_table.public_route_table.id
