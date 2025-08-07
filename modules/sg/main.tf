419a46bc4a9f85cd682333eee5868aa4acee1bb5
onoxtuti <onozawa.tsubasa@gmail.com>
2025-08-07 10:24:56 +0900

    all open sg egress

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -19,3 +19,9 @@ resource "aws_security_group_rule" "ingress" {
   description       = "SSH from ${each.value}"
   depends_on = [aws_security_group.sg]
 }
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
