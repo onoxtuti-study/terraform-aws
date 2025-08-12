705ccc546660748f8823d2113111bd4ee8bd5d50
onoxtuti <onozawa.tsubasa@gmail.com>
2025-08-12 11:28:37 +0900

    fix:gwタイプを識別し値によってkeyを変更する処理を追加

--- modules/routetable/main.tf
+++ modules/routetable/main.tf
@@ -8,7 +8,8 @@ resource "aws_route_table" "rt" {
 resource "aws_route" "default_route" {
   route_table_id         = aws_route_table.rt.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = var.gw_id  # NAT Gateway ID (nat-xxxx) も OK
+  gateway_id             = var.gw_type == "igw" ? var.gw_id : null
+  nat_gateway_id = var.gw_type == "natgw" ? var.gw_id : null
 }
 
 resource "aws_route_table_association" "rt" {
