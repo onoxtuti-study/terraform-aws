db678d127ce253da296e9cc75af0655f1a6ebf1c
onoxtuti <onozawa.tsubasa@gmail.com>
2025-08-26 10:22:00 +0900

    create:コンテナ定義を作成

--- modules/ecs/variables.tf
+++ modules/ecs/variables.tf
@@ -1,27 +1,27 @@
-# variable "container_name" {
-#   type = string
-# }
+variable "container_name" {
+  type = string
+}
 
-# variable "service_name" {
-#   type = string
-# }
+variable "service_name" {
+  type = string
+}
 
-# variable "image_url" {
-#   type = string
-# }
+variable "image_url" {
+  type = string
+}
 
-# variable "iam_arn" {
-#   type = string
-# }
+variable "iam_arn" {
+  type = string
+}
 
-# variable "subnets_id" {
-#   type = list(string)
-# }
+variable "subnets_id" {
+  type = list(string)
+}
 
-# variable "sg_id" {
-#   type = list(string)
-# }
+variable "sg_id" {
+  type = list(string)
+}
 
-# variable "alb_target_group_arn" {
-#   type = string
-# }
+variable "alb_target_group_arn" {
+  type = string
+}
